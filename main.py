c0ad049606028da8983deb4543cf2f7c77b4caae
jalakoo <jalakoo@gmail.com>
2024-08-30 12:42:56 -0700

    Working topics extraction for titles and employer info

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from typing import List, Optional
 from neo4j import GraphDatabase, basic_auth
 from pydantic import BaseModel, Field
 import functions_framework
+from openai import OpenAI
+import json
 import os
 import logging
 
@@ -12,6 +14,7 @@ HOST = os.environ.get("NEO4J_URI")
 PASSWORD = os.environ.get("NEO4J_PASSWORD")
 USER = os.environ.get("NEO4J_USER", "neo4j")
 DATABASE = os.environ.get("NEO4J_DATABASE", "neo4j")
+OPENAI_KEY = os.environ.get("OPENAI_API_KEY")
 
 
 class Location(BaseModel):
@@ -22,15 +25,27 @@ class Location(BaseModel):
 class DiffBotTime(BaseModel):
     timestamp: Optional[int] = None
 
+class Employer(BaseModel):
+    name: Optional[str] = None
+    type: Optional[str] = None
 
 class Employment(BaseModel):
     isCurrent: bool
-    employer: Optional[dict] = None
+    employer: Optional[Employer] = None
     description: Optional[str] = None
     location: Optional[Location] = None
     title: Optional[str] = None
     to: Optional[DiffBotTime] = None
 
+class Institution(BaseModel):
+    name: Optional[str] = None
+    diffbotUri: Optional[str] = None
+
+class Education(BaseModel):
+    institution: Optional[dict] = None
+    isCurrent: Optional[bool] = None
+    from_: Optional[DiffBotTime] = None
+    to: Optional[DiffBotTime] = None
 
 class DiffBotName(BaseModel):
     firstName: Optional[str] = None
@@ -40,9 +55,11 @@ class DiffBotName(BaseModel):
 class Entity(BaseModel):
     diffbotUri: Optional[str] = None
     nameDetail: Optional[DiffBotName] = None
+    description: Optional[str] = None
     origins: List[str] = Field(default_factory=list)
     summary: Optional[str] = None
     languages: List[dict] = Field(default_factory=list)
+    educations: List[Education] = Field(default_factory=list)
     employments: List[Employment] = Field(default_factory=list)
 
     def get_languages(self) -> List[str]:
@@ -56,6 +73,74 @@ class Data(BaseModel):
 class DiffBotData(BaseModel):
     data: List[Data] = Field(default_factory=list)
 
+CLIENT = OpenAI(
+    api_key=OPENAI_KEY,
+)
+
+def extract_topics(sentence: str) -> list[str]:
+
+    # Return empty list if sentence is empty
+    if not sentence or sentence.strip() == "":
+        return []
+
+    prompt = """Return a JSON List of any application names, software technologies, or programming languages from a user statement of the following structure: 
+
+    {
+    "application": [...list of applications]
+    }
+    
+    For example, return {"application": ["NextJS", "Django", "PostgresSQL"]} from the sentence "NextJS + Django + PostgreSQL" or the sentence "I am most comfortable with NextJS, Django, and PostgresSQL"."""
+
+    response = CLIENT.chat.completions.create(
+        model="gpt-4o",
+        response_format={"type": "json_object"},
+        messages=[
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": sentence},
+        ],
+        max_tokens=200,
+        n=1,
+        stop=None,
+        temperature=0.0,
+    )
+
+    extracted = response.choices[0].message.content
+
+    # print(f"Extract topics response from LLM: {extracted}")
+
+    extracted = json.loads(extracted)
+
+    # LLM may return a dictionary instead of the requested list
+    if isinstance(extracted, dict):
+        aggregate = []
+        # LLM may return several lists within a dictionary.
+        for key in list(extracted.keys()):
+            a_list = extracted[key]
+
+            # Skip any key-value payloads that aren't a list of strings
+            if not isinstance(a_list, list) and not all(
+                isinstance(item, str) for item in a_list
+            ):
+                print(
+                    f"Skipping dictionary payload '{a_list}' that is not entirely a list of strings."
+                )
+                continue
+
+            # Aggregate all the results
+            aggregate.extend(a_list)
+
+        extracted = aggregate
+
+    if not isinstance(extracted, list) or not all(
+        isinstance(item, str) for item in extracted
+    ):
+        raise ValueError(
+            f"Expected list of strings was not returned from the LLM: {extracted}"
+        )
+
+    # print(f"Topics extracted: {extracted}")
+
+    return extracted
 
 def execute_query(query, params):
     try:
@@ -114,11 +199,45 @@ def ingest(dbd: DiffBotData, tenant_id: str):
         print(f"Error creating user-tenant relationship: {e}")
         return "Error creating user-tenant relationship", 500
 
-    # Create employer nodes
+    # Extract topics from user description
+    topics = extract_topics(dbd.data[0].entity.description)
+    query = """
+    UNWIND $topics AS topic
+    MERGE (t:Topic {name: topic})
+    """
+    params = {
+        "topics": topics,
+    }
+    try:
+        execute_query(query, params)
+    except Exception as e:
+        print(f"Error creating topic nodes: {e}")
+        return "Error creating topic nodes", 500
+    
+
+    # Create User-Topic relationships
+    query = """
+    UNWIND $topics AS topic
+    MATCH (u:User {diffbotUri: $diffbotUri}), (t:Topic {name: topic})
+    MERGE (u)-[:KNOWS]->(t)
+    """
+    params = {
+        "diffbotUri": dbd.data[0].entity.diffbotUri,
+        "topics": topics,
+    }
+
+    try:
+        execute_query(query, params)
+    except Exception as e:
+        print(f"Error creating user-topic relationships: {e}")
+        return "Error creating user-topic relationships", 500
+
+    # Create Employer and Role nodes
     query = """
     UNWIND $employments AS employment
     MERGE (e:Employer {name: employment["employer"]["name"]})
     ON CREATE SET e.description = employment["description"]
+    MERGE (r:Role {name: employment["title"]})
     """
     params = {
         "employments": [e.model_dump() for e in dbd.data[0].entity.employments],
@@ -128,6 +247,75 @@ def ingest(dbd: DiffBotData, tenant_id: str):
     except Exception as e:
         print(f"Error creating employer nodes: {e}")
         return "Error creating employer nodes", 500
+
+    # Create a list of tuples with employer name and tech topics from employer descriptions
+    employer_tech = [
+        (e.employer.name, extract_topics(e.description))
+        for e in dbd.data[0].entity.employments
+        if e.employer is not None
+    ]
+    print(f'Employer tech: {employer_tech}')
+
+    # Create a list of tuples with title and tech topics from title
+    title_tech = [
+        (e.title, extract_topics(e.title))
+        for e in dbd.data[0].entity.employments
+    ]
+
+    # Get all unique tech topics from the employer tech dictionary and title tech
+    unique_tech_topics = set()
+    for _, tech_list in employer_tech:
+        unique_tech_topics.update(tech_list)
+    for _, tech_list in title_tech:
+        unique_tech_topics.update(tech_list)
+
+    # Create Tech nodes for each unique tech topic
+    query = """
+    UNWIND $unique_tech_topics AS tech_topic
+    MERGE (t:Tech {name: tech_topic})
+    """
+    params = {
+        "unique_tech_topics": list(unique_tech_topics),
+    }
+    try:
+        execute_query(query, params)
+    except Exception as e:
+        print(f"Error creating tech nodes: {e}")
+        return "Error creating tech nodes", 500
+
+
+    # Create Tech-Employer relationships
+    query = """
+    UNWIND $employer_tech AS employer_tech
+    UNWIND employer_tech[1] AS tech_topic
+    MATCH (e:Employer {name: employer_tech[0]}), (t:Tech {name: tech_topic})
+    MERGE (e)-[:USES]->(t)
+    """
+    params = {
+        "employer_tech": employer_tech,
+    }
+    try:
+        execute_query(query, params)
+    except Exception as e:
+        print(f"Error creating tech-employer relationships: {e}")
+        return "Error creating tech-employer relationships", 500    
+
+
+    # Create Tech-Title relationships
+    query = """
+    UNWIND $title_tech AS title_tech
+    UNWIND title_tech[1] AS tech_topic
+    MATCH (t:Tech {name: tech_topic}), (r:Role {name: title_tech[0]})
+    MERGE (r)-[:USES]->(t)
+    """
+    params = {
+        "title_tech": title_tech,
+    }
+    try:
+        execute_query(query, params)
+    except Exception as e:
+        print(f"Error creating tech-title relationships: {e}")
+        return "Error creating tech-title relationships", 500
 
     # Create employment relationships
     query = """
@@ -140,13 +328,33 @@ def ingest(dbd: DiffBotData, tenant_id: str):
         "employments": [e.model_dump() for e in dbd.data[0].entity.employments],
     }
 
-    print(f"employment params: {params}")
-
     try:
         execute_query(query, params)
     except Exception as e:
         print(f"Error creating employment relationships: {e}")
         return "Error creating employment relationships", 500
+
+    # Create role relationships
+    query = """
+    UNWIND $employments AS employment
+    MATCH (u:User {diffbotUri: $diffbotUri}), (r:Role {name: employment["title"]})
+    MERGE (u)-[:WAS]->(r)
+    """
+    params = {
+        "diffbotUri": dbd.data[0].entity.diffbotUri,
+        "employments": [e.model_dump() for e in dbd.data[0].entity.employments],
+    }
+
+    try:
+        execute_query(query, params)
+    except Exception as e:
+        print(f"Error creating role relationships: {e}")
+        return "Error creating role relationships", 500
+
+    # TODOS:
+    # Create Location Nodes
+
+    # Create location relationships
 
     return "OK", 200
 
